7a63aa1ddd749efc04784a01ff8167f0c1a4ec3a
David Nguyen <davebox309@gmail.com>
2021-11-26 10:42:53 -0500

    added getters to Animal Abstract'

--- src/main/java/com/zipcodewilmington/froilansfarm/Animal/Horse.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Animal/Horse.java
@@ -6,15 +6,11 @@ import com.zipcodewilmington.froilansfarm.Rideable;
 
 public class Horse extends Animal implements Rideable {
 
-    private String name;
 
     public Horse(String name) {
         super(name);
     }
 
-    public String getName(){
-        return name;
-    }
 
     public String makeNoise() {
         return "Neigh!";
