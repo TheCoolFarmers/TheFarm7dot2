360a1e6136837583b54f0d02d6461ca00817cebb
junior <junior@zipcodes-MacBook-Pro-5.local>
2021-11-26 14:19:15 -0500

    made some changes to Farm class

--- src/main/java/com/zipcodewilmington/froilansfarm/Structure/Structure.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Structure/Structure.java
@@ -1,6 +1,7 @@
 package com.zipcodewilmington.froilansfarm.Structure;
 
 public interface Structure <AnyAnimal>{
+
     void add(AnyAnimal anyAnimal);
     void remove(AnyAnimal anyAnimal);
     AnyAnimal getByName(String name);
